c2b148b0d518a9d4632e91bdfdc5228da3cb8e44
Shaun Niel Ochavo <shaunniel02@gmail.com>
2023-11-18 20:39:28 +0800

    Convert MapComponent into dynamic

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import Navbar from "@/components/Navbar";
 import HomeComponent from "@/components/Home";
 import AboutComponent from "@/components/About";
-import MapComponent from "@/components/Map";
 import Footer from "@/components/Footer";
 
+import dynamic from "next/dynamic";
+
 export default function Home() {
+  const MapComponent = dynamic(() => import("@/components/Map"), {
+    ssr: false,
+  });
+
   return (
     <>
       <div className="container mx-auto w-full fixed top-0 flex justify-end z-50">
